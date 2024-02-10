4ccbb051fddeeb86db247c454e5055670415a425
dharamraj-dev <dharamrajyadav6312@gmail.com>
2024-02-10 18:05:17 +0530

    First Day

---
+++ FIrst-Day/first.cpp
@@ -0,0 +1,106 @@
+#include <iostream> //Header File
+using namespace std;
+
+// Code for Print Value
+int main()
+{
+    cout<<"India";
+    return 0;
+}
+
+// Code for Operators assign
+
+// Addition
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,b,sum;
+  a = 10;
+  b = 10;
+  sum = a+b;
+  cout<<sum;
+  return 0;
+}
+
+
+// Subtraction
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    a = 10;
+    b = 20;
+
+    c = b - a;
+    cout<<c;
+}
+
+
+// Multiplaction
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+
+    a = 10;
+    b = 20;
+
+    c = a*b;
+    cout<<c;
+}
+
+// Divide
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+
+    a = 10;
+    b = 20;
+
+    c = a/b;
+    cout<<c;
+}
+
+// Remaindar
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    a = 10;
+    b = 20;
+    c = a%b;
+
+    cout<<c;
+}
+
+
+// Code for Data types 
+
+// String
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a,b,c;
+
+    a = "My";
+    b = "India";
+
+    c = a + b;
+    cout<<c;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+}
